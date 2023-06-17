1610d8832b912cf48d6464ad89b0d7ad0a47035a
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-06-17 22:18:31 +0200

    add ip anonymization logging

--- src/proxy.js
+++ src/proxy.js
@@ -1,16 +1,20 @@
 const http = require('http');
 const conf = require('../config/http.conf');
 
-const targetHost = conf.server.host;
+const targetHost = process.env.TARGET_HOST || conf.server.host;
 const targetPort = conf.server.port;
 
 const proxy = http.createServer((req, res) => {
+  console.log(`client: ${ req.socket.remoteAddress }`);
+
   const proxyReq = http.request({
     host: targetHost,
     port: targetPort,
     method: req.method,
     path: req.url,
-    headers: req.headers
+    headers: {
+      'x-forwarded-for': req.socket.remoteAddress,
+    }
   }, (proxyRes) => {
     res.writeHead(proxyRes.statusCode, proxyRes.headers);
     proxyRes.pipe(res);
--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const http = require('http');
 
-const server = http.createServer((_req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+const server = http.createServer((req, res) => {
+  const client = req.headers['x-forwarded-for'];
 
+  if (client) {
+    console.log(`client: ${ client }`);
+    console.log(`proxy: ${ req.socket.remoteAddress }`);
+  } else {
+    console.log(`client: ${ req.socket.remoteAddress }`);
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   const data = {
     message: 'Example Relay Proxy',
   };
-
   res.write(JSON.stringify(data));
-  
   res.end();
 });
 
